19b8cbb48a23eb686acc8ecb93c0126f300bd751
pli2014 <116342437@qq.com>
2015-03-15 12:14:00 +0800

    安装脚本

--- config.php
+++ config.php
@@ -13,14 +13,14 @@
 define('iPHP_DB_TYPE','mysql');//数据库类型 mysql sqlite (SQLite3)
 define('iPHP_DB_HOST','localhost');// 服务器名或服务器ip,一般为localhost
 define('iPHP_DB_USER','root');// 数据库用户
-define('iPHP_DB_PASSWORD','123456');//数据库密码
-define('iPHP_DB_NAME','test');// 数据库名
+define('iPHP_DB_PASSWORD','fib%yel5');//数据库密码
+define('iPHP_DB_NAME','cms');// 数据库名
 define('iPHP_DB_PREFIX','icms_');// 表名前缀, 同一数据库安装多个请修改此处
 define('iPHP_DB_CHARSET','utf8');//MYSQL编码设置.如果您的程序出现乱码现象，需要设置此项来修复. 请不要随意更改此项，否则将可能导致系统出现乱码现象
 define('iPHP_DB_PREFIX_TAG','#iCMS@__');// SQL表名前缀替换
 //define('iPHP_DB_COLLATE', 	'');
 //----------------------------------------
-define('iPHP_KEY','n9pSQYvdWhtBz3UHZFVL7c6vf4x6fePk');
+define('iPHP_KEY','vEK6Z7kubs5wp75ss6UXHRHk4n3eL6YT');
 define('iPHP_CHARSET','utf-8');
 //---------------cookie设置-------------------------
 define('iPHP_COOKIE_DOMAIN','');
