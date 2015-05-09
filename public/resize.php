f36a550d413e234bd7c540de318b02fddde2e3cb
pli2014 <116342437@qq.com>
2015-05-09 15:45:56 +0800

    图片的压缩和媒体查找自适应！

---
+++ public/resize.php
@@ -0,0 +1,25 @@
+<?php
+require dirname(__file__) . '/../iCMS.php';
+require dirname(__file__) . '/resizeimage.php';
+if (isset($_GET['file'])) {
+    $path = trim($_GET['file']); //新图路径
+    $srcPath = substr($path, 0, strpos($path, "_")); //原图路径
+    $size = substr($path, strpos($path, "_") + 1);
+    if (strpos($path, "_")>0 && is_file(iPATH . $srcPath)) {
+        $ext = strtolower(strrchr($srcPath, '.'));
+        if ($ext == '.jpg' || $ext == '.jpeg') {
+            header('content-type:image/jpg');
+        } elseif ($ext == '.png') {
+            header('content-type:image/png');
+        } else {
+            header('HTTP/1.1 404 Not Found');
+        }
+        if(!is_file(iPATH . $path)){
+            //就能生成缩略图,其中,源文件和缩略图地址可以相同,140,140分别代表宽和高
+            $widthHeight = explode("x",$size);
+            $resizeimage = new resizeimage(iPATH . $srcPath, $widthHeight[0], $widthHeight[1], "0", iPATH . $path);
+        }
+        echo file_get_contents(iPATH . $path);
+        exit;
+    }
+}
